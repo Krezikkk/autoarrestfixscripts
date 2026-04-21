6c1dc4bf669241a0491674aebc7f8cd81cc6d80d
Krez <wokkekrezik@gmail.com>
2026-04-21 23:44:18 +0300

    Refactor script loading to use load_script function
    Replaced direct loading of scripts with a dedicated function to handle loading and error reporting.

--- loader.lua
+++ loader.lua
@@ -1,12 +1,36 @@
 --[[
-          _    _ _______ ____             _____  ______  _____ _______   _____     _______ _____ _    _ 
-     /\  | |  | |__   __/ __ \      /\   |  __ \|  ____|/ ____|__   __| |  __ \ /\|__   __/ ____| |  | |
-    /  \ | |  | |  | | | |  | |    /  \  | |__) | |__  | (___    | |    | |__) /  \  | | | |    | |__| |
-   / /\ \| |  | |  | | | |  | |   / /\ \ |  _  /|  __|  \___ \   | |    |  ___/ /\ \ | | | |    |  __  |
-  / ____ \ |__| |  | | | |__| |  / ____ \| | \ \| |____ ____) |  | |    | |  / ____ \| | | |____| |  | |
- /_/    \_\____/   |_|  \____/  /_/    \_\_|  \_\______|_____/   |_|    |_| /_/    \_\_|  \_____|_|  |_|
-                                                                                                        
+          _    _ _______ ____             _____  ______  _____ _______   _____     _______ _____ _    _ 
+     /\  | |  | |__   __/ __ \      /\   |  __ \|  ____|/ ____|__   __| |  __ \ /\|__   __/ ____| |  | |
+    /  \ | |  | |  | | | |  | |    /  \  | |__) | |__  | (___    | |    | |__) /  \  | | | |    | |__| |
+   / /\ \| |  | |  | | | |  | |   / /\ \ |  _  /|  __|  \___ \   | |    |  ___/ /\ \ | | | |    |  __  |
+  / ____ \ |__| |  | | | |__| |  / ____ \| | \ \| |____ ____) |  | |    | |  / ____ \| | | |____| |  | |
+ /_/    \_\____/   |_|  \____/  /_/    \_\_|  \_\______|_____/   |_|    |_| /_/    \_\_|  \_____|_|  |_|
 ]]
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Krezikkk/autoarrestfixscripts/refs/heads/main/killparts%20on%20stuck%20places.lua'))()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Krezikkk/autoarrestfixscripts/refs/heads/main/server%20hopping%20script%20for%20inactivity%20detection.lua'))()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Krezikkk/autoarrestfixscripts/refs/heads/main/serverhop%20if%20detected%20roblox%20kick%20gui.lua'))()
+
+if not game:IsLoaded() then
+    print("waiting for game to load...")
+    game.Loaded:Wait()
+end
+print("game fully loaded, starting script execution")
+
+task.wait(7)
+print("7 seconds delay finished, starting to load external scripts")
+
+local function load_script(url)
+    print("attempting to load: " .. url)
+    local success, err = pcall(function()
+        loadstring(game:HttpGet(url))()
+    end)
+    
+    if success then
+        print("successfully loaded and executed: " .. url)
+    else
+        print("error while loading " .. url .. ": " .. tostring(err))
+    end
+end
+
+load_script('https://raw.githubusercontent.com/Krezikkk/autoarrestfixscripts/refs/heads/main/killparts%20on%20stuck%20places.lua')
+load_script('https://raw.githubusercontent.com/Krezikkk/autoarrestfixscripts/refs/heads/main/server%20hopping%20script%20for%20inactivity%20detection.lua')
+load_script('https://raw.githubusercontent.com/Krezikkk/autoarrestfixscripts/refs/heads/main/serverhop%20if%20detected%20roblox%20kick%20gui.lua')
+
+print("all scripts processed, check f9 console for errors")
