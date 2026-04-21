f737f14579c83e368e5ad4fb72a51a7f32c26592
Krez <wokkekrezik@gmail.com>
2026-04-21 23:41:58 +0300

    patch loader

---
+++ loader.lua
@@ -0,0 +1,12 @@
+--[[
+          _    _ _______ ____             _____  ______  _____ _______   _____     _______ _____ _    _ 
+     /\  | |  | |__   __/ __ \      /\   |  __ \|  ____|/ ____|__   __| |  __ \ /\|__   __/ ____| |  | |
+    /  \ | |  | |  | | | |  | |    /  \  | |__) | |__  | (___    | |    | |__) /  \  | | | |    | |__| |
+   / /\ \| |  | |  | | | |  | |   / /\ \ |  _  /|  __|  \___ \   | |    |  ___/ /\ \ | | | |    |  __  |
+  / ____ \ |__| |  | | | |__| |  / ____ \| | \ \| |____ ____) |  | |    | |  / ____ \| | | |____| |  | |
+ /_/    \_\____/   |_|  \____/  /_/    \_\_|  \_\______|_____/   |_|    |_| /_/    \_\_|  \_____|_|  |_|
+                                                                                                        
+]]
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Krezikkk/autoarrestfixscripts/refs/heads/main/killparts%20on%20stuck%20places.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Krezikkk/autoarrestfixscripts/refs/heads/main/server%20hopping%20script%20for%20inactivity%20detection.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Krezikkk/autoarrestfixscripts/refs/heads/main/serverhop%20if%20detected%20roblox%20kick%20gui.lua'))()
